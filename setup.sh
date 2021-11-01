7a3f6f8d955cf30f7819547163d8c1d527f25111
paix26875 <shimada2022@gmail.com>
2021-11-01 15:45:54 +0900

    コメントの追加とログファイルの作成

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 echo 'Setting up Now .....'
+# アーカイブディレクトリが存在しない場合アーカイブディレクトリを作成
 if [ ! -d 'archive' ]; then
     mkdir 'archive'
 fi
 
+# 前回の実験データが残っている場合、そのデータをアーカイブする
 if [ -d 'temp' ]; then
     echo 'Archiving the previous data .....'
-    today=`date '+%Y%m%d'`
-    archive_dir="archive/${today}"
+    # windowsだとsedコマンドがないかもしれない
+    # 参考：https://qiita.com/hirohiro77/items/7fe2f68781c41777e507
+    previous_experiment_date=`sed -n 1p temp/log.csv`
+    archive_dir="archive/${previous_experiment_date}"
     if [ -d $archive_dir ]; then
         echo 'Do you want to remove a directory that already exists? [yes/no(recommend)]'
         read answer
@@ -23,9 +27,14 @@ if [ -d 'temp' ]; then
     fi
 fi
 
+# 今日の実験データの保存先
 echo 'Creating new directory .....'
+today=`date '+%Y%m%d'`
 mkdir 'temp'
 cp 'setting.py.example' 'temp/setting.py'
 cd 'temp'
+touch 'log.txt'
+touch 'data.csv'
+echo ${today} > log.txt
 mkdir 'images'
 mkdir 'calibrationimg'
